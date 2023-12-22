6a9d1793ba612f8f3506f201cd71f08c5c3fe093
Carlos Marx <carlosmarx27@gmail.com>
2023-12-22 20:00:34 -0300

    Build

---
+++ public/build/assets/ListItem-2217575c.js
@@ -0,0 +1,1 @@
+import{a as e,F as r,j as l}from"./app-c5272f55.js";import s from"./CheckIcon-27a9d4df.js";function i(t){return e(r,{children:l("li",{className:"flex gap-2 text-2xl items-center my-2 bg-slate-50 py-3 pl-3 lg:pr-16 pr-6 sm:w-full rounded-xl",children:[e(s,{}),t.item]})})}export{i as default};
