58bf59b7a70371446b472e1b3785e64bc7298df9
Carlos Marx <carlosmarx27@gmail.com>
2023-12-23 18:39:44 -0300

    Link kit caderneta run build correcao

---
+++ public/build/assets/ListItem-a928a873.js
@@ -0,0 +1,1 @@
+import{a as e,F as r,j as l}from"./app-38306a5d.js";import s from"./CheckIcon-621f7ce4.js";function i(t){return e(r,{children:l("li",{className:"flex gap-2 text-2xl items-center my-2 bg-slate-50 py-3 pl-3 lg:pr-16 pr-6 sm:w-full rounded-xl",children:[e(s,{}),t.item]})})}export{i as default};
