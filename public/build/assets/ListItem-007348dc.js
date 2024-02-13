e689d1596a5c67e558504ef45351dc84d6620dbf
Carlos Marx <carlosmarx27@gmail.com>
2024-02-13 10:31:48 -0300

    npm run build

--- public/build/assets/ListItem-007348dc.js
+++
@@ -1,1 +0,0 @@
-import{a as e,F as r,j as l}from"./app-b708e0ee.js";import s from"./CheckIcon-91499cab.js";function i(t){return e(r,{children:l("li",{className:"flex gap-2 text-2xl items-center my-2 bg-slate-50 py-3 pl-3 lg:pr-16 pr-6 sm:w-full rounded-xl",children:[e(s,{}),t.item]})})}export{i as default};
